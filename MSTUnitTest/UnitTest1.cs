c5f51669ac284b24b68091d8e3a7fa6d11a44f49
ananthss2007 <ananth_ss@hotmail.com>
2020-06-08 18:26:32 -0400

    initial commit

---
+++ MSTUnitTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SampleWebAPI.Controllers;
+
+namespace MSTUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        ValuesController controller = new ValuesController();
+        [TestMethod]
+        public void GetReturnByName()
+        {
+
+            var returnValue = controller.Get(1);
+            Assert.AreEqual("Ananth Subramoniam", returnValue.ToString());
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
